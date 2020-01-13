c31c7e6672473984cff334c92024a330f331bd56
coexists <37199113+coexists@users.noreply.github.com>
2020-01-13 18:22:25 -0500

    Lab1

---
+++ Lab1/index.js
@@ -0,0 +1,5 @@
+const Person = require("./person.js");
+
+let p = new Person("Test", "Blue");
+console.log(p.name);
+console.log(p.color);
---
+++ Lab1/person.js
@@ -0,0 +1,13 @@
+class Person {
+    constructor(name, color) {
+        this.name = name;
+        this.color = color;
+    }
+
+speak() {
+    console.log("HI");
+}
+
+}
+
+module.exports = Person;
