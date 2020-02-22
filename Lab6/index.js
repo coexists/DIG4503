56f59fb1b98438dd1e3c737004dda3dbd790dcae
coexists <37199113+coexists@users.noreply.github.com>
2020-02-22 00:28:09 -0500

    wow it works?

--- Lab6/index.js
+++ Lab6/index.js
@@ -1,19 +1,18 @@
 const Express = require("express");
 const App = Express();
+const cors = require("cors"); 
 const port = 80; 
 const chalk = require("Chalk");
 
-// const pokemons = require("json-pokemon");
-const getPokemons = require("json-pokemon");
+const coolPokemons = require("json-pokemon");
 
-//cors allows the dif ports to communicate with each other
 App.use(cors());
-const cors = require("cors"); 
+
 App.get("/", Express.static("client/build"));
 
 App.get("/id/:id", (req,res) => {
-    let result = {"error": "Sorry, there were no matches"};
-    getPokemons.forEach((value) =>{
+    let result = {"error": "Invalid Pokemon ID!"};
+    coolPokemons.forEach((value) =>{
         if(value.id == req.params.id){
             result = value;
         }
@@ -23,14 +22,13 @@ App.get("/id/:id", (req,res) => {
     }
     else{
         console.log(chalk.green(req.path));
-        console.log(result);
     }
     res.send(result);
 });
 
 App.get("/name/:name", (req,res) => {
-    let result = {"error": "Sorry, there were no matches"};
-    getPokemons.forEach((value) =>{
+    let result = {"error": "Invalid Pokemon Name!"};
+    coolPokemons.forEach((value) =>{
         if(value.name.toLowerCase() == req.params.name){
          result = value;
         }
@@ -40,11 +38,10 @@ App.get("/name/:name", (req,res) => {
     }
     else{
         console.log(chalk.green(req.path));
-        console.log(result);
     }
     res.send(result);
 });
 
 App.listen(port, () =>{
-    console.log("Server is up and ruuuuuuuunning");
+    console.log("Server is a' Runnin'");
 })
